46e70114697ce14b87d45a40161f4a93650a9719
ghostwolfs <420129823@qq.com>
2015-10-07 19:25:35 +0800

    changes

--- 前端/login.js
+++ 前端/login.js
@@ -5,9 +5,11 @@ function demo(){
 	var a=document.getElementById("usrName").value;
 	var len=getlength(a);
 	if(a==""){
-      alert("用户名不能为空！");
+	  alert("用户名不能为空")
+      return false;
 	}
 	else if(len>30) {
 	  alert("用户名不能超过30个字符");
+	  return false;
 	}
 }
