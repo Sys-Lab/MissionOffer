fc1d0de91a263e61f5525645f6d83ae149ca35fc
ghostwolfs <420129823@qq.com>
2015-10-22 17:53:38 +0800

    register

--- 前端/注册界面/注册页面/register.js
+++ 前端/注册界面/注册页面/register.js
@@ -1,3 +1,4 @@
+var isValidate=false;
 function getlength(str){
         return str.length;
       }
@@ -31,13 +32,36 @@ function getlength(str){
           document.getElementById("input1_explain").innerHTML="邮箱不能为空！";
           document.getElementById("input1_explain").style.color="red";
           document.getElementById("input1_explain").style.top="230px";
+          document.getElementById("uN").focus();
+          isValidate=false;
+          return;
         }
         else{
-          document.getElementById("input1_p").innerHTML="";
-          document.getElementById("input1_p").style.background="url(2.jpg)";
-          document.getElementById("input1_p").style.top="225px";
-          document.getElementById("input1_explain").innerHTML="";
-        };
+          isValidate=true;
+          return;
+        }
+        if(isValidate){
+          $.get('', {
+             '' : document.getElementById('uN').value
+              }, function(document.getElementById("uN")) {
+                 if (document.getElementById("uN")) {
+                   document.getElementById("input1_p").innerHTML="";
+                   document.getElementById("input1_p").style.background="url(2.jpg)";
+                   document.getElementById("input1_p").style.top="225px";
+                   document.getElementById("input1_explain").innerHTML="";
+                   } else {
+                   document.getElementById("input1_p").innerHTML="";
+                   document.getElementById("input1_p").style.background="url(1.jpg)";
+                   document.getElementById("input1_p").style.top="230px";
+                   document.getElementById("input1_explain").innerHTML="该邮箱已注册！";
+                   document.getElementById("input1_explain").style.color="red";
+                   document.getElementById("input1_explain").style.top="230px";
+                   document.getElementById("uN").focus();
+                   isValidate=false;
+                   return;
+                   }
+            });
+        }  
       }
       function input2(){
         document.getElementById("input2_explain").style.display="block";
@@ -52,6 +76,8 @@ function getlength(str){
           document.getElementById("input2_explain").innerHTML="密码不能为空!";
           document.getElementById("input2_explain").style.top="320px";
           document.getElementById("input2_explain").style.color="red";
+          document.getElementById("pW").focus();
+          isValidate=false;
         }
         else if(/ /g.test(a)){
           document.getElementById("input2_p").innerHTML="";
@@ -60,6 +86,8 @@ function getlength(str){
           document.getElementById("input2_explain").innerHTML="密码不能含有空格!";
           document.getElementById("input2_explain").style.top="320px";
           document.getElementById("input2_explain").style.color="red";
+          document.getElementById("pW").focus();
+          isValidate=false;
         }
         else if(b<6||b>16){
           document.getElementById("input2_p").innerHTML="";
@@ -68,12 +96,15 @@ function getlength(str){
           document.getElementById("input2_explain").innerHTML="密码长度为6-16！";
           document.getElementById("input2_explain").style.top="320px";
           document.getElementById("input2_explain").style.color="red";
+          document.getElementById("pW").focus();
+          isValidate=false;
         }
         else{
           document.getElementById("input2_p").innerHTML="";
           document.getElementById("input2_p").style.background="url(2.jpg)";
           document.getElementById("input2_p").style.top="320px";
           document.getElementById("input2_explain").innerHTML="";
+          isValidate=true;
         };
       }
       function input3(){
@@ -89,12 +120,15 @@ function getlength(str){
           document.getElementById("input3_explain").innerHTML="密码不正确!";
           document.getElementById("input3_explain").style.color="red";
           document.getElementById("input3_explain").style.top="420px";
+          document.getElementById("rP").focus();
+          isValidate=false;
         }
         else{
           document.getElementById("input3_p").innerHTML="";
           document.getElementById("input3_p").style.background="url(2.jpg)";
           document.getElementById("input3_p").style.top="420px";
           document.getElementById("input3_explain").innerHTML="";
+          isValidate=true;
         }
       }
       function input4(){
@@ -111,6 +145,8 @@ function getlength(str){
           document.getElementById("input4_explain").innerHTML="昵称不能为空!";
           document.getElementById("input4_explain").style.top="515px";
           document.getElementById("input4_explain").style.color="red";
+          document.getElementById("nN").focus();
+          isValidate=false;
         }
         else if(/ /g.test(a)||re.test(a)){
           document.getElementById("input4_p").innerHTML="";
@@ -119,6 +155,8 @@ function getlength(str){
           document.getElementById("input4_explain").innerHTML="昵称不能含有空格和非法字符!";
           document.getElementById("input4_explain").style.top="515px";
           document.getElementById("input4_explain").style.color="red";
+          document.getElementById("nN").focus();
+          isValidate=false;
         }
         else if(b<6||b>16){
           document.getElementById("input4_p").innerHTML="";
@@ -127,12 +165,15 @@ function getlength(str){
           document.getElementById("input4_explain").innerHTML="昵称长度为6-16！";
           document.getElementById("input4_explain").style.top="515px";
           document.getElementById("input4_explain").style.color="red";
+          document.getElementById("nN").focus();
+          isValidate=false;
         }
         else{
           document.getElementById("input4_p").innerHTML="";
           document.getElementById("input4_p").style.background="url(2.jpg)";
           document.getElementById("input4_p").style.top="510px";
           document.getElementById("input4_explain").innerHTML="";
+          isValidate=true;
         };
       }
       function input5(){
@@ -140,6 +181,7 @@ function getlength(str){
       }
       function input5_judge(){
         var a=document.getElementById("sN").value;
+        var b=getlength(a);
         if(a==""){
           document.getElementById("input5_p").innerHTML="";
           document.getElementById("input5_p").style.background="url(1.jpg)";
@@ -147,12 +189,25 @@ function getlength(str){
           document.getElementById("input5_explain").innerHTML="学号不能为空！";
           document.getElementById("input5_explain").style.color="red";
           document.getElementById("input5_explain").style.top="680px";
+          document.getElementById("sN").focus();
+          isValidate=false;
+        }
+        else if(b!=13){
+          document.getElementById("input5_p").innerHTML="";
+          document.getElementById("input5_p").style.background="url(1.jpg)";
+          document.getElementById("input5_p").style.top="675px";
+          document.getElementById("input5_explain").innerHTML="学号格式不正确!";
+          document.getElementById("input5_explain").style.top="680px";
+          document.getElementById("input5_explain").style.color="red";
+          document.getElementById("sN").focus();
+          isValidate=false;
         }
         else{
           document.getElementById("input5_p").innerHTML="";
           document.getElementById("input5_p").style.background="url(2.jpg)";
           document.getElementById("input5_p").style.top="675px";
           document.getElementById("input5_explain").innerHTML="";
+          isValidate=true;
         };
       }
       function input6(){
@@ -160,6 +215,7 @@ function getlength(str){
       }
       function input6_judge(){
         var a=document.getElementById("pN").value;
+        var b=getlength(a);
         if(a==""){
           document.getElementById("input6_p").innerHTML="";
           document.getElementById("input6_p").style.background="url(1.jpg)";
@@ -167,11 +223,26 @@ function getlength(str){
           document.getElementById("input6_explain").innerHTML="手机号不能为空！";
           document.getElementById("input6_explain").style.color="red";
           document.getElementById("input6_explain").style.top="775px";
+          document.getElementById("pN").focus();
+          isValidate=false;
+        else if(b!=11){
+          document.getElementById("input6_p").innerHTML="";
+          document.getElementById("input6_p").style.background="url(1.jpg)";
+          document.getElementById("input6_p").style.top="770px";
+          document.getElementById("input6_explain").innerHTML="手机号格式不正确！";
+          document.getElementById("input6_explain").style.top="775px";
+          document.getElementById("input6_explain").style.color="red";
+          document.getElementById("pN").focus();
+          isValidate=false;
         }
         else{
           document.getElementById("input6_p").innerHTML="";
           document.getElementById("input6_p").style.background="url(2.jpg)";
           document.getElementById("input6_p").style.top="770px";
           document.getElementById("input6_explain").innerHTML="";
+          isValidate=true;
         };
+      }
+      function submit(){
+        return isValidate;
       }
