7a37099c74d6a1594d7b8f36857bfe0cc7d907fc
ghostwolfs <420129823@qq.com>
2015-10-25 16:23:00 +0800

    div name

--- MissionOffer/statics/frameworkFolder/framework.js
+++ MissionOffer/statics/frameworkFolder/framework.js
@@ -9,18 +9,21 @@
       document.getElementById("all_a").style.color="#979797";
       document.getElementById("done_a").style.color="#979797";
       document.getElementById("get_a").style.color="#979797";
+      document.getElementById("task_condition").name="进行中";
     }
     function done_demo(){
       document.getElementById("done_a").style.color="#64C864";
       document.getElementById("all_a").style.color="#979797";
       document.getElementById("ing_a").style.color="#979797";
       document.getElementById("get_a").style.color="#979797";
+      document.getElementById("task_condition").name="已完成";
     }
     function get_demo(){
       document.getElementById("done_a").style.color="#979797";
       document.getElementById("all_a").style.color="#979797";
       document.getElementById("ing_a").style.color="#979797";
       document.getElementById("get_a").style.color="#64C864";
+      document.getElementById("task_condition").name="待接收";
     }
     function lost_p(){
       document.getElementById("boss1022").src="/statics/frameworkFolder/lost1_pressed.png";
@@ -33,6 +36,7 @@
       document.getElementById("boss104").style.left="23%";
       document.getElementById("boss105").style.left="21.5%";
       document.getElementById("boss106").style.left="22%";
+      document.getElementById("classify").name="寻找失物";
     }
     function ding_p(){
       document.getElementById("boss1022").src="/statics/frameworkFolder/lost_pressed.png";
@@ -45,6 +49,7 @@
       document.getElementById("boss104").style.left="23%";
       document.getElementById("boss105").style.left="21.5%";
       document.getElementById("boss106").style.left="22%";
+      document.getElementById("classify").name="食堂带饭";
     }
     function super_p(){
       document.getElementById("boss1022").src="/statics/frameworkFolder/lost_pressed.png";
@@ -57,6 +62,7 @@
       document.getElementById("boss104").style.left="32%";
       document.getElementById("boss105").style.left="21.5%";
       document.getElementById("boss106").style.left="22%";
+      document.getElementById("classify").name="超市购物";
     }
     function print_p(){
       document.getElementById("boss1022").src="/statics/frameworkFolder/lost_pressed.png";
@@ -69,6 +75,7 @@
       document.getElementById("boss104").style.left="23%";
       document.getElementById("boss105").style.left="30.5%";
       document.getElementById("boss106").style.left="22%";
+      document.getElementById("classify").name="文印中心";
     }
     function out_p(){
       document.getElementById("boss1022").src="/statics/frameworkFolder/lost_pressed.png";
@@ -81,6 +88,7 @@
       document.getElementById("boss104").style.left="23%";
       document.getElementById("boss105").style.left="21.5%";
       document.getElementById("boss106").style.left="31%";
+      document.getElementById("classify").name="校外任务";
     }
     function dp(){
       document.getElementById("display").style.display="block";
